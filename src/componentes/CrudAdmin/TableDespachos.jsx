354ad2ffe24647c948816c7bd0267d3778a50e48
Remi Garcia Herrera <rem.garcia@duocuc.cl>
2026-05-30 10:04:43 -0400

    se agregan los puertos de conexion a la instancia de ricardo

--- src/componentes/CrudAdmin/TableDespachos.jsx
+++ src/componentes/CrudAdmin/TableDespachos.jsx
@@ -8,7 +8,7 @@ export const TableDespachos = () => {
 
   const despacho = async () => {
     await axios
-      .get("http://192.168.3.20/api/v1/despachos", {
+      .get("http://3.239.214.107:8081/api/v1/despachos", {
         headers:{
               'Content-Type': 'application/json',
               'Accept': 'application/json'
